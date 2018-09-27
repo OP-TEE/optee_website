b55874af65aef80e7c32c832250acc2d175c356c
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-09-27 10:34:11 +0100

    Added the fly.js file to the main and sticky js packages

--- assets/js/package-main.js
+++ assets/js/package-main.js
@@ -1,10 +1,11 @@
 //= require vendor/jquery
 //= require vendor/bootstrap
-//= require vendor/cookieconsent
 //= require vendor/mc
+//= require vendor/cookieconsent
 //= require vendor/lazysizes
 
 //= require app/main
 //= require app/tables
+//= require app/sticky-tab-bar
+//= require app/custom
 //= require app/fly
-
